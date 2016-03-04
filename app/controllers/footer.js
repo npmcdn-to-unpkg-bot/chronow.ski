92b1f7156889ff242b9f7a62d83b4894f8910da6
Jakub Chronowski <jakub@chronow.ski>
2016-03-04 14:16:09 +0100

    Add VKontakte to the footer

--- app/controllers/footer.js
+++ app/controllers/footer.js
@@ -22,6 +22,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 function FooterComponent() {
                     this.sociallinks = [
                         { "name": "Facebook", "icon": "facebook", "url": "https://facebook.com/kuba77" },
+                        { "name": "VKontakte", "icon": "vk", "url": "https://vk.com/chronowski" },
                         { "name": "LinkedIn", "icon": "linkedin", "url": "https://linkedin.com/pub/jakub-chronowski/109/80a/810" },
                         { "name": "Twitter", "icon": "twitter", "url": "https://twitter.com/jakubchronowski" },
                         { "name": "Instagram", "icon": "instagram", "url": "https://instagram.com/19kuba22" },
