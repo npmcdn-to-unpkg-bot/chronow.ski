52119f31be3bcddb746ca5d9f3eb577ceba577f1
Jakub Chronowski <jakub@chronow.ski>
2016-03-07 16:52:35 +0100

    Move angular application to 'client' directory

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,39 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var css = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
 
 var sys = require('sys')
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-
 gulp.task('sass', function() {
-  gulp.src('./app/sass/app.sass')
+  gulp.src('./client/app/sass/app.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(css())
     .pipe(concat('app.min.css'))
-    .pipe(gulp.dest('./assets/'));
+    .pipe(gulp.dest('./client/assets/'));
 });
 
-var ie_dependencies = [
-  'node_modules/es6-shim/es6-shim.min.js',
-  'node_modules/systemjs/dist/system-polyfills.js',
-  'node_modules/angular2/es6/prod/src/testing/shims_for_IE.js',
-]
-
-var dependencies = [
-  'node_modules/angular2/bundles/angular2-polyfills.min.js',
-  'node_modules/systemjs/dist/system.js',
-  'node_modules/rx-lite/rx.lite.min.js',
-  'node_modules/angular2/bundles/router.min.js',
-  'node_modules/angular2/bundles/angular2.min.js',
-]
-
 gulp.task('default', function() {
-  gulp.watch('./app/sass/*.sass', ['sass']);
+  gulp.watch('./client/app/sass/*.sass', ['sass']);
   gulp.watch('./**/*.ts', function() {
     exec("tsc", puts);
   });
